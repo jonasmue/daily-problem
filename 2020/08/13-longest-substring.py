d3e547e0e5bde2256e7d4812233fea1408d1b06b
Jonas Müller <jonas.mueller@compitencies.com>
2020-08-13 21:10:24 +0200

    Add sliding window solution

--- 2020/08/13-longest-substring.py
+++ 2020/08/13-longest-substring.py
@@ -14,5 +14,24 @@ class Solution:
                 break
         return max(counter, self.naiveLengthOfLongestSubstring(s[1:]))
 
+    def slidingLengthOfLongestSubstring(self, s):
+        if not len(s):
+            return 0
+        if len(s) == 1:
+            return 1
+        characters = set(s[0])
+        slow_runner = 0
+        fast_runner = 1
+        max_len = 0
+        while fast_runner < len(s):
+            if s[fast_runner] in characters:
+                characters.remove(s[slow_runner])
+                slow_runner += 1
+            else:
+                characters.add(s[fast_runner])
+                fast_runner += 1
+            max_len = max(max_len, fast_runner - slow_runner)
+        return max_len
 
-print(Solution().naiveLengthOfLongestSubstring('abrkaabcdefghijjxxx'))
+
+print(Solution().slidingLengthOfLongestSubstring('abrkaabcdefghijklmjabcdefghijklmnopxxx'))
